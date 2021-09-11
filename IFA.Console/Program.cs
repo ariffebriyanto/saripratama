3afb38fd206dd1640518d9d3da2c9847ceab62d7
ariffebriyanto <83152525+ariffebriyanto@users.noreply.github.com>
2021-09-11 22:09:57 +0700

    Add files via upload

---
+++ IFA.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+namespace IFA.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+           // string sql = "EXEC AutoReverse ";
+
+            DynamicParameters param = new DynamicParameters();
+          //  var res = con.Query<int>(sql, param).FirstOrDefault();
+        }
+    }
+}
