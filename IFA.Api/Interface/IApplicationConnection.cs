302c48466234b20dbcbcd68f17fc253e7253720e
ariffebriyanto <83152525+ariffebriyanto@users.noreply.github.com>
2021-09-11 22:27:54 +0700

    Add files via upload

---
+++ IFA.Api/Interface/IApplicationConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Api.Interface
+{
+    public interface IApplicationConnection : IDisposable
+    {
+        IDbConnection Connection { get; }
+        IDbTransaction Transaction { get; }
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+
+        IDbCommand CreateCommand();
+        IEnumerable<dynamic> Query(string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null);
+
+        IEnumerable<T> Query<T>(string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null);
+
+        int Execute(string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null);
+    }
+}
---
+++ IFA.Api/Interface/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Api.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //OVP
+        #region Request
+        //IRequestDocumentErrorRepo RequestDocumentErrorRepo { get; }
+        //IRequestDocumentRepo RequestDocumentRepo { get; }
+        //IRequestRepo RequestRepo { get; }
+        #endregion
+
+        #region DMERequest
+       // IDMERequestRepo dMERequestRepo { get; }
+        //IDMEReverseRepo dMEReverseRepo { get; }
+        #endregion
+
+        void BeginTransaction();
+
+        void CommitTransaction();
+
+        void RollbackTransaction();
+    }
+}
