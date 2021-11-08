f1b6b07748e1cc056ddb35b59e6c011282ab6eda
arif febriyanto <83152525+ariffebriyanto@users.noreply.github.com>
2021-11-08 09:33:34 +0700

    Add files via upload

---
+++ IFA.Web/wwwroot/Script/Master/Harga.js
@@ -0,0 +1,281 @@
+﻿var Hargads = [];
+var urlAction = "";
+var optionsGrid = {
+    pageSize: 15
+};
+$(document).ready(function () {
+
+    $("#files").kendoUpload({
+        async: {
+            saveUrl: urlSaveUpload,
+            autoUpload: false
+        }
+    });
+
+
+
+    startSpinner('Loading..', 1);
+    $.when(getData()).done(function () {
+        bindGrid();
+        startSpinner('loading..', 0);
+    });
+});
+
+function uploadSelected() {
+    $(".k-upload-selected").click();
+}
+
+
+function getData() {
+    var urlLink = urlGetData;
+
+    return $.ajax({
+        url: urlLink,
+        success: function (result) {
+            Hargads = [];
+            Hargads = result;
+            console.log(Hargads);
+        },
+        error: function (data) {
+            alert('Something Went Wrong');
+            startSpinner('loading..', 0);
+        }
+    });
+}
+
+function bindGrid() {
+    $("#GridKota").kendoGrid({
+        columns: [
+
+            { field: "kode_Barang", title: "Kode Barang", width: "100px",filterable: false, },
+            { field: "nama_Barang", title: "Nama Barang", width: "300px" },
+            { field: "harga_Rupiah", title: "Harga 1", width: "100px", filterable: false, },
+            { field: "harga_Rupiah2", title: "Harga 2", width: "160px", filterable: false, },
+            { field: "harga_Rupiah3", title: "Harga 3", width: "100px", filterable: false, },
+            { field: "harga_Rupiah4", title: "Harga 4", width: "100px", filterable: false, },
+            { field: "qty_harga1_min", title: "Min Qty Harga 1", width: "100px", filterable: false, },
+            { field: "qty_harga2_min", title: "Min Qty Harga 2", width: "100px", filterable: false, },
+            { field: "qty_harga3_min", title: "Min Qty Harga 3", width: "100px", filterable: false, },
+            { field: "harga_RupiahOld", title: "Copy Harga 1", width: "100px",hidden:true},
+            { field: "harga_RupiahOld2", title: "Copy Harga 1", width: "100px",hidden: true },
+            { field: "harga_RupiahOld3", title: "Copy Harga 1", width: "100px",hidden: true },
+            { field: "selisih", title: "selisih", width: "100px",hidden: true },
+            { field: "selisih2", title: "selisih2", width: "100px",hidden: true },
+            { field: "selisih3", title: "selisih2", width: "100px", hidden: true },
+            { field: "updateStatus", title: "selisih2", width: "100px", hidden: true },
+
+        
+        ],
+        dataSource: {
+            data: Hargads,
+            schema: {
+                model: {
+                    id: "Kode_Barang",
+                    fields: {
+                        kode_Barang: { type: "string", editable: false },
+                        nama_Barang: { type: "string", editable: false },
+                        harga_Rupiah: { type: "number", editable: true },
+                        harga_Rupiah2: { type: "number", editable: true },
+                        harga_Rupiah3: { type: "number", editable: true },
+                        harga_Rupiah4: { type: "number", editable: true },
+                        qty_harga1_min: { type: "number", editable: true },
+                        qty_harga2_min: { type: "number", editable: true },
+                        qty_harga3_min: { type: "number", editable: true },
+                        selisih: { type: "string", editable: true },
+                        selisih2: { type: "string", editable: true },
+                        selisih3: { type: "string", editable: true },
+                        selisih4: { type: "string", editable: true },
+                        harga_RupiahOld: { type: "number", editable: true },
+                        harga_RupiahOld2: { type: "number", editable: true },
+                        harga_RupiahOld3: { type: "number", editable: true },
+                        harga_RupiahOld4: { type: "number", editable: true },
+                        updateStatus: { type: "string", editable: true }
+                      
+                    }
+                }
+            },
+            change: function (e) {
+                if (e.field === "harga_Rupiah") {
+                    // The total is Prix (price) * Quantite (Quantity)
+                    var newTotal = e.items[0].harga_Rupiah - e.items[0].harga_RupiahOld;
+                    var Status = "";
+                    if (newTotal > 0) {
+                        Status = "Harga Naik Rp " + newTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    else {
+                        Status = "Harga Turun Rp " + Math.abs(newTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    console.log("Nouveau total : " + newTotal);
+                    e.items[0].set("selisih", Status);
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "harga_Rupiah2") {
+                    // The total is Prix (price) * Quantite (Quantity)
+                    var newTotal = e.items[0].harga_Rupiah2 - e.items[0].harga_RupiahOld2;
+                    var Status = "";
+                    if (newTotal > 0) {
+                        Status = "Harga Naik Rp " + newTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    else {
+                        Status = "Harga Turun Rp " + Math.abs(newTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    console.log("Nouveau total : " + newTotal);
+                    e.items[0].set("selisih2", Status);
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "harga_Rupiah3") {
+                    // The total is Prix (price) * Quantite (Quantity)
+                    var newTotal = e.items[0].harga_Rupiah3 - e.items[0].harga_RupiahOld3;
+                    var Status = "";
+                    if (newTotal > 0) {
+                        Status = "Harga Naik Rp " + newTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    else {
+                        Status = "Harga Turun Rp " + Math.abs(newTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    console.log("Nouveau total : " + newTotal);
+                    e.items[0].set("selisih3", Status);
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "harga_Rupiah4") {
+                    // The total is Prix (price) * Quantite (Quantity)
+                    var newTotal = e.items[0].harga_Rupiah4 - e.items[0].harga_RupiahOld4;
+                    var Status = "";
+                    if (newTotal > 0) {
+                        Status = "Harga Naik Rp " + newTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    else {
+                        Status = "Harga Turun Rp " + Math.abs(newTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                    console.log("Nouveau total : " + newTotal);
+                    e.items[0].set("selisih4", Status);
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "qty_harga1_min") {
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "qty_harga2_min") {
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "qty_harga3_min") {
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+                else if (e.field === "qty_harga4_min") {
+                    e.items[0].set("updateStatus", "Y");
+
+                }
+            },
+            pageSize: optionsGrid.pageSize
+        },
+         filterable: {
+        extra: false,
+        operators: {
+            string: { contains: "Contains" }
+        }
+    },
+        editable: true,
+        groupable: true,
+        sortable: true,
+        pageable: {
+            pageSizes: [5, 10, 20, 100],
+            change: function () {
+                prepareActionGrid();
+            }
+        },
+        toolbar: ["excel"],
+        excel: {
+            fileName: "ExportHarga.xlsx", allPages: true, Filterable:true
+        },
+        noRecords: true
+    }).data("kendoGrid");
+
+}
+
+function onSaveClicked() {
+    save();
+}
+
+function save() {
+    gridData = $("#GridKota").data("kendoGrid");
+    //paramValue = gridData.dataSource.data().toJSON();
+
+    //var models = [];
+    //gridData.table.find("input[type=checkbox]:checked").each(function () {
+    //    var row = $(this).closest("tr");
+    //    var model = gridData.dataItem(row);
+    //    models.push(model);
+    //});
+  
+    var saveData = gridData.dataSource.data().toJSON();
+    var dataSave = $.grep(saveData, function (v) {
+        return v.updateStatus === "Y" ;
+    });
+
+   
+ 
+ 
+
+    //var savedata = {
+
+    //};
+    //  console.log('savedata: ' + JSON.stringify(models));
+    swal({
+        type: 'warning',
+        title: 'Are you sure?',
+        html: 'You want to submit this data',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d9534f'
+    }).then(function (isConfirm) {
+        if (isConfirm.value === true) {
+            startSpinner('loading..', 1);
+
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(dataSave),
+                dataType: "json",
+                url: urlSave,
+                contentType: 'application/json; charset=utf-8',
+                success: function (result) {
+                    if (result.success === false) {
+                        Swal.fire({
+                            type: 'error',
+                            title: 'Warning',
+                            html: result.message
+
+                        });
+                    } else {
+                        Swal.fire({
+                            type: 'success',
+                            title: 'Success',
+                            html: result.message
+                        });
+                        
+                        $('#GridKota').kendoGrid('destroy').empty();
+                      
+                        $.when(getData()).done(function () {
+                            bindGrid();
+                        });
+                    }
+                    startSpinner('loading..', 0);
+
+
+
+                },
+                error: function (data) {
+                    alert('Something Went Wrong');
+                    startSpinner('loading..', 0);
+                }
+            });
+        }
+    });
+}
+
+
---
+++ IFA.Web/wwwroot/Script/Master/Role.js
@@ -0,0 +1,273 @@
+﻿var brgDS = [];
+var urlAction = "";
+var GridPeg;
+var optionsGrid = {
+    pageSize: 20
+};
+$(document).ready(function () {
+    dataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: urlGetData
+            },
+            update: {
+
+                type: "POST",
+                url: urlUpdate,
+                dataType: "json",
+                contentType: "application/json",
+                complete: function (e) {
+                    $("#GridPeg").data("kendoGrid").dataSource.read();
+                }
+
+            },
+            destroy: {
+                type: "POST",
+                url: urlDelete,
+                dataType: "json",
+                contentType: "application/json",
+                complete: function (e) {
+                    $("#GridPeg").data("kendoGrid").dataSource.read();
+                }
+
+            },
+            create: {
+                type: "POST",
+                url: urlSave,
+                dataType: "json",
+                contentType: "application/json",
+                complete: function (e) {
+                    $("#GridPeg").data("kendoGrid").dataSource.read();
+                }
+
+            },
+            parameterMap: function (option, operation) {
+                if (operation !== "read") {
+                    return kendo.stringify(option.models);
+                }
+            }
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "IDUSER",
+                fields: {
+                    iduser: { type: "string", validation: { required: true } },
+                    iduser: { type: "string", validation: { required: true } }
+
+
+
+                }
+            }
+        }
+    });
+
+    GridPeg = $("#GridPeg").kendoGrid({
+        dataSource: dataSource,
+        groupable: true,
+        sortable: true,
+        filterable: {
+            extra: false,
+            operators: {
+                string: { contains: "Contains" }
+            }
+        },
+        toolbar: ["excel"],
+        excel: {
+            fileName: "ExportHarga.xlsx", allPages: true, Filterable: true
+        },
+        pageable: true,
+        height: 550,
+        requestEnd: onRequestEnd,
+        toolbar: [{
+            name: "create",
+            text: "Tambah Role"
+        }],
+        columns: [
+            { field: "iduser", title: "USER", width: "100px", readonly: true  }, //, editor: barangDropDownEditor
+            { field: "idrole", title: "Role", width: "100px", editor: ListDropDownEditor },
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "70px" }],
+        editable: "inline"
+    }).data("kendoGrid");
+    //console.log(JSON.stringify(dataSource));
+    //console.log(dataSource);
+
+    //$("#cabang_gudang").kendoDropDownList({
+    //    dataTextField: "nama",
+    //    dataValueField: "kd_cabang",
+    //    filter: "contains",
+    //    dataSource: GudangList,
+    //    value: GudangUser,
+    //    change: changegudang,
+    //    enable: false,
+    //    optionLabel: "Please Select"
+    //});
+});
+
+function ListDropDownEditor(container, options) {
+    var input = $('<input id="value" name="text">');
+    input.appendTo(container);
+
+    input.kendoDropDownList({
+        valuePrimitive: true,
+        dataTextField: "text",
+        dataValueField: "text",
+        dataSource: Datalist,
+        filter: "contains",
+        //optionLabel: "Select Barang",
+
+        template: "<span data-id='${data.value}' data-Approval='${data.text}'>${data.text}</span>",
+        select: function (e) {
+            var id = e.item.find("span").attr("data-id");
+            var approval = GridPeg.dataItem($(e.sender.element).closest("tr"));
+            approval.idrole = id;
+        }
+    }).appendTo(container);
+}
+
+var Datalist = [
+    { text: "", value: "" },
+    { text: "PENJUALAN", value: "PENJUALAN" },
+    { text: "UAT", value: "UAT" },
+    { text: "SPV", value: "SPV" },
+    { text: "SPV1", value: "SPV1" }
+];
+
+//function barangDropDownEditor(container, options) {
+//    var input = $('<input required id="nama" name="nama">');
+//    input.appendTo(container);
+
+//    input.kendoDropDownList({
+//        valuePrimitive: true,
+//        dataTextField: "nama",
+//        dataValueField: "nama",
+//        dataSource: GudangList,
+//        optionLabel: "Select Cabang...",
+//        filter: "contains",
+//        virtual: {
+//            valueMapper: function (options) {
+//                options.success([options.nama || 0]);
+//            }
+//        },
+//        template: "<span data-id='${data.kd_Cabang}' data-Barang='${data.nama}'>${data.nama}</span>",
+//        select: function (e) {
+//            var id = e.item.find("span").attr("data-id");
+//            var Barang = e.item.find("span").attr("data-Barang");
+//            kd = id;
+//            var barang = GridPeg.dataItem($(e.sender.element).closest("tr"));
+//            barang.kd_Cabang = id;
+//            barang.nama = Barang;
+//            console.log(JSON.stringify(GudangList));
+//            //var found = GetBarangDetail(id);
+//            //barang.satuan = found[0].kd_Satuan;
+//            //barang.stok = found[0].stok;
+//            //barang.harga = found[0].harga_Rupiah;
+//            //barang.vol = found[0].vol;
+
+
+
+//        }
+//    }).appendTo(container);
+//}
+
+
+function onRequestEnd(e) {
+    if (e.type === "create") {
+        e.sender.read();
+    }
+    else if (e.type === "update") {
+        e.sender.read();
+    }
+}
+
+function getData() {
+    var urlLink = urlGetData;
+
+    return $.ajax({
+        url: urlLink,
+        success: function (result) {
+            brgDS = [];
+            brgDS = result;
+            console.log(JSON.stringify(brgDS));
+            console.log(JSON.stringify(result));
+        },
+        error: function (data) {
+            alert('Something Went Wrong');
+            startSpinner('loading..', 0);
+        }
+    });
+}
+
+function prepareActionGrid() {
+    $(".hapusData").on("click", function () {
+        var id = $(this).data("id");
+        deleteKota(id);
+    });
+    $(".editData").on("click", function () {
+        var id = $(this).data("id");
+        showForm(id);
+    });
+}
+
+
+function showlist() {
+    $("#editForm").css("display", "none");
+    $("#wrapperList").css("display", "");
+}
+
+function onSaveClicked() {
+    urlAction = urlSave;
+    validationPage();
+}
+
+function onUpdateClicked() {
+    urlAction = urlUpdate;
+    validationPage();
+}
+
+function validationPage() {
+    var reWhiteSpace = /\s/g;
+    // var kode_Pegawai = $('#kode_Pegawai').val();
+    var id_role = $('#idrole').val();
+    var id_user = $('#iduser').val();
+    // var Keterangan = $('#Keterangan').val();
+
+    //validationMessage = '';
+    //if (!kode_Pegawai) {
+    //    validationMessage = validationMessage + 'Kode Pegawai harus di isi.' + '\n';
+    //}
+    if (!id_role) {
+        validationMessage = validationMessage + 'id_user  harus di isi.' + '\n';
+    }
+
+    if (!id_user) {
+        validationMessage = validationMessage + 'id_user harus di isi.' + '\n';
+    }
+
+    //if (Kode_Kota.match(reWhiteSpace)) {
+    //    validationMessage = validationMessage + 'Kode Kota tidak boleh ada spasi.' + '\n';
+    //}
+    if (validationMessage) {
+        Swal.fire({
+            type: 'error',
+            title: 'Warning',
+            html: validationMessage
+        });
+    }
+    else {
+        $.when(SaveData()).done(function (x) {
+            if (typeof x !== "undefined") {
+                $('#GridPeg').kendoGrid('destroy').empty();
+                Swal.fire({
+                    type: 'success',
+                    title: 'Success',
+                    html: "Data Tersimpan.."
+                });
+                getData();
+                showlist();
+            }
+
+        });
+    }
+}
